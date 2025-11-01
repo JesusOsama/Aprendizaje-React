50b4d80ed06fa8773341e8c8ddee6d4c0e559382
JesusOsama <jesusv2911@gmail.com>
2025-11-01 11:53:36 -0500

    Let, const, string

---
+++ src/bases/01-const-let.ts
@@ -0,0 +1,11 @@
+
+const firstName: string = 'Fernando';
+const lastName: string = 'Herrera';
+
+let diceNumber = 5;
+diceNumber = 3;
+
+console.log(firstName, lastName);
+
+const containsLetterH = lastName.includes('h')
+console.log({containsLetterH, diceNumber})
