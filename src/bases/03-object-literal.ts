8305d1f9d43022b783329db0774338f90bfafabf
JesusOsama <jesusv2911@gmail.com>
2025-11-02 10:43:47 -0500

    Interfaces

--- src/bases/03-object-literal.ts
+++ src/bases/03-object-literal.ts
@@ -1,21 +1,41 @@
-const ironman = {
+interface Person {
+    firstName: string;
+    lastName: string;
+    age: number;
+    address?: Address;
+}
+
+interface Address { 
+    postalCode: string;
+        city: string;
+}
+
+const ironman: Person = {
     firstName: 'Tony',
     lastName: 'Stark',
     age: 45,
-    adress: {
-        postalCode: 'ABC123',
+    address: {
+        postalCode: 'ABC-123',
         city: 'New York'
     }
 };
 
+const spiderman: Person ={
+    firstName: "Peter",
+    lastName: "Parker",
+    age: 22
+}
+
+console.log(ironman)
+
 //const spiderman = { ... ironman}; Solo copia de un nivel
 
-const spiderman = structuredClone(ironman); // Copia todos los nivele
+// const spiderman = structuredClone(ironman); // Copia todos los nivele
 
-spiderman.firstName = 'Peter';
-spiderman.lastName = 'Parker';
-spiderman.age = 22;
-spiderman.adress.city = 'San José'
+// spiderman.firstName = 'Peter';
+// spiderman.lastName = 'Parker';
+// spiderman.age = 22;
+// spiderman.adress.city = 'San José'
 
 
-console.log(spiderman, ironman)
+// console.log(spiderman, ironman)
