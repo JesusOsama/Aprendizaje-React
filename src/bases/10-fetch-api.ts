720b39d616fd33e144a3612f3d4c65a814e28f44
JesusOsama <jesusv2911@gmail.com>
2025-11-09 12:17:30 -0500

    Interface Giphy

--- src/bases/10-fetch-api.ts
+++ src/bases/10-fetch-api.ts
@@ -1,19 +1,23 @@
+import type { GiphyRandomResponse } from "../data/giphy.response";
+
 const API_KEY = "OzzahoMBVXyyU0tuq8aJCVLcDqyfkm7o";
 
 const myRequest = fetch(
     `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`
 );
 
+const createImageInsideDOM = (url: string) =>{
+   const imgElement = document.createElement('img');
+   imgElement.src = url;
+   document.body.append(imgElement) 
+};
+
+
 myRequest
 .then((response) => response.json())
-.then((data) => {
-    const imageUrl = data.data.images.original.url;;
-    console.log(imageUrl);
-
-    const imgElement = document.createElement('img');
-    imgElement.src = imageUrl;
-
-    document.body.append(imgElement)
+.then(({data}: GiphyRandomResponse) => {
+    const imageUrl = data.images.original.url;
+    createImageInsideDOM(imageUrl)
 })
 .catch ((err) => {
     console.error(err);
